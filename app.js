356e73c6847aab582849794968197ea4aa22df07
Christopher Bolivar <christopher@Julios-MacBook-Pro.local>
2020-05-10 01:07:57 -0400

    added models

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config()
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+// import routes
+const userRoutes = require('./routes/user');
 
-//import routes
-const userRoutes = require('./routes/user')
+// app
+const app = express();
 
-//app
-const app = express()
-//db
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-}).then(()=>{
-    console.log("DB connected")
-})
+// db
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('DB Connected'));
 
-//routes middleware
-app.use(userRoutes)
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
 
-const port = process.env.PORT || 5000
+// routes middleware
+app.use('/api', userRoutes);
+const port = process.env.PORT || 8000;
 
-app.listen(port, ()=>{
-    console.log(`You are running on port: ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
